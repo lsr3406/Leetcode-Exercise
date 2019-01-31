919df49aebbe0742be54b59ef3333cc8d9fed914
siru <lsr3406@163.com>
2019-01-31 11:10:25 +0800

    完成 345 最大蓄水问题

---
+++ 345-Container-With-Most-Water/C++/main.cpp
@@ -0,0 +1,77 @@
+/**
+ * encoding: utf-8
+ * @author: siru
+ * @create on: 2019-01-31 10:24:31
+ * @update on: 2019-01-31 11:06:15
+ */
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+
+        // return maxArea1(height);
+        return maxArea2(height);
+    }
+
+    /**
+     * 方案一: 二重遍历, 找出所有的可能情况
+     * 时间复杂度: O(n^2)
+     * 空间复杂度: O(1)
+     */ 
+    int maxArea1(vector<int>& height) {
+
+        int tempMaxArea = 0;
+        for (int i = 0; i < height.size(); i ++) {
+            for (int j = i + 1; j < height.size(); j ++) {
+                int currentArea = (j - i) * min(height[i], height[j]);
+                if ( currentArea > tempMaxArea )
+                    tempMaxArea = currentArea;
+            }
+        }
+        return tempMaxArea;
+    }
+
+    /**
+     * 方案二: 贪心算法, 指针对撞, 可使用反证法确定该方案能得到最优解
+     * 时间复杂度: O(n)
+     * 空间复杂度: O(1)
+     */ 
+    int maxArea2(vector<int>& height) {
+
+        int tempMaxArea = 0;
+        int l = 0, r = height.size() - 1;
+
+        while (l < r) {
+            // 计算当前面积
+            int currentArea = (r - l) * min(height[l], height[r]);
+            if (currentArea > tempMaxArea)
+                tempMaxArea = currentArea;
+
+            // TODO prove
+            if (height[l] > height[r])
+                r --;
+            else  // height[l] <= height[r]
+                l ++;
+        }
+        return tempMaxArea;
+    }
+};
+
+int main(int argc, char const *argv[]) {
+    
+    int arr[] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+    vector<int> height(arr, end(arr));
+
+    int res = Solution().maxArea(height);
+
+    cout << res << endl;
+
+    return 0;
+}
+
