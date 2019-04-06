cfd34252471479d97240a0ad3ae7e585f81b0ccd
siru <lsr3406@163.com>
2019-04-06 20:39:58 +0800

    添加广度优先遍历法

--- 0279-Perfect-Squares/Java/src/Solution.java
+++ 0279-Perfect-Squares/Java/src/Solution.java
@@ -2,38 +2,72 @@
  * encoding: utf-8
  * @author: siru
  * @create on: 2019-04-06 18:04:14
- * @update on: 2019-04-06 18:48:11
+ * @update on: 2019-04-06 20:36:59
  */
 import java.lang.Math;
+import java.util.Queue;
 import java.util.Map;
+import java.util.LinkedList;
 import java.util.HashMap;
 
 class Solution {
 
-    private Map<Integer, Integer> record;
+    // private Map<Integer, Integer> record;
 
-    private int getNumSquares(int n) {
+    // // 暴力搜索递归函数
+    // private int getNumSquares(int n) {
 
-        if (record.containsKey(n))
-            return record.get(n);
+    //     if (record.containsKey(n))
+    //         return record.get(n);
 
-        int sq = 1;
-        int res = n;
-        for (int i = 1; sq <= n; i ++, sq = i*i) {
-            res = Math.min(getNumSquares(n - sq) + 1, res);
-        }
-        record.put(n, res);
-        return res;
-    }
+    //     int res = n;
+    //     for (int i = (int) Math.sqrt(n); i > 0; i --) {
+    //         res = Math.min(getNumSquares(n - i * i) + 1, res);
+    //     }
+    //     record.put(n, res);
+    //     return res;
+    // }
 
+    // // 暴力搜索
+    // public int numSquares(int n) {
+
+    //     if (n == 0)
+    //         return 0;
+
+    //     record = new HashMap<>();
+
+    //     return getNumSquares(n);
+    // }
+
+    // 广度优先遍历法
     public int numSquares(int n) {
 
         if (n == 0)
             return 0;
 
-        record = new HashMap<>();
+        Queue<Integer> queue = new LinkedList<>();
+        int res = 0;
 
-        return getNumSquares(n);
+        queue.offer(n);
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+            res ++;
+
+            for (int i = 0; i < size; i ++) {
+                int cur = queue.poll();
+                int num = (int) Math.sqrt(cur);
+
+                for (int j = num; j > 0; j --) {
+                    int next = cur - j * j;
+
+                    if (next == 0)
+                        return res;
+                    queue.offer(next);
+                }
+            }
+        }
+        return 0;
     }
 
     public static void main(String[] args) {
